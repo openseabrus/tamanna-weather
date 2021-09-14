33dfdde994ba5e7abe2146da09f9572f16e21db8
Guilherme Seabra <guilherme.seabra@diconium.com>
2021-09-14 21:08:31 +0100

    feat: persist redux state

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import './index.scss';
 import App from './components/app/App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { saveState } from './redux/storePersister';
+import throttle from 'lodash/throttle';
+
+store.subscribe(
+	throttle(() => {
+		saveState({
+			cities: store.getState().cities,
+		});
+	}, 1000)
+);
 
 ReactDOM.render(
 	<Provider store={store}>
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,12 @@ import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
+import { loadState } from './storePersister';
+
+const persistedState = loadState();
 
 export default createStore(
 	rootReducer,
+	persistedState,
 	composeWithDevTools(applyMiddleware(thunk))
 );
