8fddd86c8acfa3fdef1223f8bafdf1ad7bd54110
Guilherme Seabra <guilherme.seabra@diconium.com>
2021-09-14 22:22:40 +0100

    feat: header icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,12 +3,38 @@ import PropTypes from 'prop-types';
 
 export const Header = ({ title }) => {
 	return (
-		<header className="header">
-			<img className="header__logo" src="/assets/tamanna.png" alt="Tamanna" />
-			<nav className="header__navigation">
-				<h1>{title}</h1>
-			</nav>
-		</header>
+		<nav className="header">
+			<a href="#">
+				<img
+					className="header__logo"
+					src="/assets/tamanna.png"
+					alt="Tamanna"
+					title="Tamanna"
+				/>
+			</a>
+			<h1 className="header__title">{title}</h1>
+			<div className="header__icons">
+				<a href="https://github.com/openseabrus" target="_blank" rel="noreferrer">
+					<img
+						className="header__icon"
+						src="/assets/github.png"
+						alt="Github"
+						title="Github"
+					/>
+				</a>
+				<a href="https://linkedin.com/in/seabrus" target="_blank" rel="noreferrer">
+					<img
+						className="header__icon"
+						src="/assets/linkedin.png"
+						alt="Linkedin"
+						title="Linkedin"
+					/>
+				</a>
+				<a href="/assets/_CV__seabrus.pdf" target="_blank" rel="noreferrer">
+					<img className="header__icon" src="/assets/cv.png" alt="CV" title="CV" />
+				</a>
+			</div>
+		</nav>
 	);
 };
 
